bafbcef7510d0914963a7e78a93df009e7929993
Bubsyyy <lubomirlubomirovkn123@gmail.com>
2025-01-16 23:11:46 +0200

    Jedi Galaxy

---
+++ src/_01_Working_With_Abstraction/JediGalaxy/Star.java
@@ -0,0 +1,13 @@
+package _01_Working_With_Abstraction.JediGalaxy;
+
+public class Star {
+    private int value;
+
+    public Star(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
