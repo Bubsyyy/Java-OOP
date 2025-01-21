93d26f430e4a622b94b1db8ca71ea6c380516057
Bubsyyy <lubomirlubomirovkn123@gmail.com>
2025-01-21 22:37:39 +0200

    food shortage

---
+++ src/_04_Interfaces_And_Abstraction/FoodShortage/Buyer.java
@@ -0,0 +1,7 @@
+package _04_Interfaces_And_Abstraction.FoodShortage;
+
+public interface Buyer extends Person {
+    void buyFood();
+
+    int getFood();
+}
---
+++ src/_04_Interfaces_And_Abstraction/FoodShortage/Identifiable.java
@@ -0,0 +1,5 @@
+package _04_Interfaces_And_Abstraction.FoodShortage;
+
+public interface Identifiable {
+    String getId();
+}
